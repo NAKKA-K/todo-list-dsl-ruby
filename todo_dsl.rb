d478b6a872abb8d1e6da5494170b9dc853c1c6d8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-19 15:03:18 +0900

    Update date format

--- todo_dsl.rb
+++ todo_dsl.rb
@@ -8,10 +8,11 @@ def todo(task_name, deadline: nil)
     now = Date.today
     task_deadline = Date.parse(deadline)
     if now > task_deadline
-        puts "[Emergency] #{task_name} の期限が切れています"
+        puts "! [Emergency] #{task_name} の期限が切れています"
+	print "締め切りから", (now - task_deadline).to_i, "日過ぎています\n"
     else
         puts "[Notification] #{task_name}"
-        print "締め切りまでの残り日数は", task_deadline - now, "日です\n"
+        print "締め切りまでの残り日数は", (task_deadline - now).to_i, "日です\n"
     end
     puts
 end
